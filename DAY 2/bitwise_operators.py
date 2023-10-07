4a4211c30c0a0f8f631d95ea77e286ea43befac0
Rathod Prashanth <108942308+rathodd07@users.noreply.github.com>
2023-10-07 09:56:25 +0530

    Add files via upload

---
+++ DAY 2/bitwise_operators.py
@@ -0,0 +1,12 @@
+print(10&4+3)
+# 4+3=7 , 7=0111 binary numbers
+#10=1010 binary numbers then 10&7=1010&0111=0010
+print(6|3&9+6)#9+6=15 then 13 ka binary no 15=1111 
+#3&15=0011&1111 =0011 the 6|3=0110|0011=111=7
+print(~9+4&6)#9+4=13 then ~13&6 now ~13=-14
+print(7<<1)# shifting one bit from right to left and ans is 14
+print(7>>1)#shifting one bit from left to right  and ans is 3
+print(1^2^3)
+print(5>>2)
+print(5<<2)
+print(9<<4)
---
+++ DAY 2/class_concept.py
@@ -0,0 +1,9 @@
+class A:
+    a=10
+    def s(self,x):
+        print(x)
+    def d(self):
+        print(self.a)
+obj=A()
+obj.s(20)
+obj.d()
