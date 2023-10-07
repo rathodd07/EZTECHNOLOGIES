feefb5780a302096a5cff448cde3dc347b090d7e
Rathod Prashanth <108942308+rathodd07@users.noreply.github.com>
2023-10-07 10:02:11 +0530

    Create order_parantheses.py

---
+++ DAY 4/order_parantheses.py
@@ -0,0 +1,18 @@
+# order printing paranthese
+def generate(n):
+    def backtract(s, left, right):
+        if len(s) == n*2:
+            res.append(s)
+            return
+        if left < n:
+            backtract(s+'(', left+1, right)
+        if right < left:
+            backtract(s+')', left, right+1)
+    res = []
+    backtract('', 0, 0)
+    return res
+    
+    
+n=int(input())
+ans = generate(n)
+print(ans)
